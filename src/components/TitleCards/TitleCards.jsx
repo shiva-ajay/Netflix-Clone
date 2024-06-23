0503d60aea743bcd99b5965679ffa1c0b6c6d9b0
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-23 12:57:20 +0530

    Cards

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
-import './TitleCards.css'
+import React, { useEffect, useRef } from "react";
+import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data";
 
-const TitleCards = () => {
+const TitleCards = ({title, category}) => {
+  const cardsRef = useRef();
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    if (cardsRef.current) {
+      cardsRef.current.scrollLeft += event.deltaY;
+    }
+  };
+
+  useEffect(() => {
+    const refCurrent = cardsRef.current;
+    if (refCurrent) {
+      refCurrent.addEventListener("wheel", handleWheel);
+    }
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      if (refCurrent) {
+        refCurrent.removeEventListener("wheel", handleWheel);
+      }
+    };
+  }, []);
+
   return (
-    <div className='titlecards'>
-
+    <div className="titlecards">
+      <h2>{title?title:"Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => (
+          <div className="card" key={index}>
+            <img src={card.image} alt={card.name} />
+            <p>{card.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TitleCards
+export default TitleCards;
