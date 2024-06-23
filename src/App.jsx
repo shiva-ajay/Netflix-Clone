6a9d8dede0cf95da071544b25fe8d383751a590c
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-23 18:07:33 +0530

    Login/API movies data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Home from './pages/Home/Home'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login/Login'
 
 const App = () => {
   return (
     <>
-    <Home />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+    </Routes>
     </>
   )
 }
--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,22 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
 import cards_data from "../../assets/cards/Cards_data";
 
 const TitleCards = ({title, category}) => {
+
+  const [apiData, setApiData] = useState([]);
+
   const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYTFhNjUxMTQ3ZjJjNDZjNTA2MTgyNWJmMjRmNjQyYSIsIm5iZiI6MTcxOTE0NDg4Mi43MTM5NzYsInN1YiI6IjY2NzgxMTA3MzdjZGFmMTQ4MDNiMjQ3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.o3EF9mxZC6XFc4MZIpA7Lx9RQcKg5G4ZuKCxwwj6X7M'
+    }
+  };
+  
+
 
   const handleWheel = (event) => {
     event.preventDefault();
@@ -13,6 +26,12 @@ const TitleCards = ({title, category}) => {
   };
 
   useEffect(() => {
+
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results))
+    .catch(err => console.error(err));
+
     const refCurrent = cardsRef.current;
     if (refCurrent) {
       refCurrent.addEventListener("wheel", handleWheel);
@@ -30,10 +49,10 @@ const TitleCards = ({title, category}) => {
     <div className="titlecards">
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => (
+        {apiData.map((card, index) => (
           <div className="card" key={index}>
-            <img src={card.image} alt={card.name} />
-            <p>{card.name}</p>
+            <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt={card.name} />
+            <p>{card.original_title}</p>
           </div>
         ))}
       </div>
